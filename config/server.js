78001d290e340bc1cd84df3a3996a227aae2185e
Dave Gillhespy <yodasw16@gmail.com>
2021-09-13 13:02:53 -0400

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '265146c1b258fc67e3323921a595cff5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3e590095-7541-4b2f-be8f-cde8fdc572d5'
+};
